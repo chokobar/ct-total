0f6fb5672c824fe616e3c6f1850c11672acfdd15
chokobar89 <chokobar89@naver.com>
2025-02-26 16:01:31 +0900

    프로그래머스 코딩테스트 입문

---
+++ src/introduction/codingTest/Test07.java
@@ -0,0 +1,14 @@
+package introduction.codingTest;
+
+public class Test07 {
+
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int num1, int num2) {
+                int answer = -1;
+                answer =num1+num2;
+                return answer;
+            }
+        }
+    }
+}
